da08f604588a2f682c99b9be7302a7af9b17d0e1
Hanslettthedev <Hanslettthedev@gmail.com>
2024-06-11 22:16:26 +0100

    html boilerplate code
    Signed-off-by: Hanslettthedev <Hanslettthedev@gmail.com>

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,15 +1,17 @@
-import {createRouter, createWebHistory} from "vue-router";
+import { createRouter, createWebHistory } from "vue-router";
 import Homepage from "../pages/Homepage.vue";
 
+const routes = [
+  {
+    path: "/",
+    name: "Home",
+    component: Homepage,
+  },
+];
+
 const router = createRouter({
-    history: createWebHistory(import.meta.env.BASE_URL),
-    routes: [
-        {
-            path: "/",
-            name: "Home",
-            component: Homepage,
-        }
-    ],
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes,
 });
 
 export default router;
